230e44bf52c76d565eab917ae776b68c37228748
Mateen.io <gbadamosimateen@gmail.com>
2023-02-11 17:29:46 +0100

    made navbar collapse on clicking nav links

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,26 +3,37 @@
 const hamburger = document.querySelector(".menu--toggle");
 const navMenu = document.querySelector(".right--side__nav");
 const navLinkContainer = document.querySelector(".nav--items__container");
+const navLinks = document.querySelectorAll(".nav-link");
 
-hamburger.addEventListener("click", function () {
+const navToggle = function () {
   navMenu.classList.toggle("showing");
   navLinkContainer.classList.toggle("showing");
-});
+};
+
+hamburger.addEventListener("click", navToggle);
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", navToggle);
+}
 
 //tab controller
 const tabs = document.querySelectorAll(".tab-btn");
 const all_content = document.querySelectorAll(".content");
 
 tabs.forEach((tab, index) => {
-    tab.addEventListener('click', (e) => {
-        tabs.forEach(tab => { tab.classList.remove('active') });
-        tab.classList.add('active');
+  tab.addEventListener("click", (e) => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
 
-        const line = document.querySelector('.line');
-        line.style.width = e.target.offsetWidth + 3 + 'px';
-        line.style.left = e.target.offsetLeft + 'px';
+    const line = document.querySelector(".line");
+    line.style.width = e.target.offsetWidth + 3 + "px";
+    line.style.left = e.target.offsetLeft + "px";
 
-        all_content.forEach(content => { content.classList.remove('active') });
-        all_content[index].classList.add('active');
-    })
-})
+    all_content.forEach((content) => {
+      content.classList.remove("active");
+    });
+    all_content[index].classList.add("active");
+  });
+});
