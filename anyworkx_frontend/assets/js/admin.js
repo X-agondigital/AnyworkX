0b8dfc045c4116d62cb43e0748ef7091214b3376
Mateen.io <gbadamosimateen@gmail.com>
2023-05-27 09:28:51 +0100

    added auto logout to admin pages

--- anyworkx_frontend/assets/js/admin.js
+++ anyworkx_frontend/assets/js/admin.js
@@ -1,19 +1,5 @@
 "use strict";
-// function checkAuth() {
-//   const adminToken = localStorage.getItem('adminToken');
-//   if (!adminToken) {
-//     // Admin is not logged in, redirect to login page
-//     window.location.href = '../admin-login.html'; // Replace with your login page URL
-//   }
-// }
 
-// function logout() {
-//   localStorage.removeItem('adminToken');
-//   // Redirect to the login page
-//   window.location.href = '/login.html'; // Replace with your login page URL
-// }
-
-// document.getElementById('logout').addEventListener('click', logout())
 // get admin token from localStorage
 const adminToken = localStorage.getItem("adminToken");
 
---
+++ anyworkx_frontend/assets/js/auth.js
@@ -0,0 +1,17 @@
+function checkAuth() {
+  const adminToken = localStorage.getItem("adminToken");
+  if (!adminToken) {
+    // Admin is not logged in, redirect to login page
+    window.location.href = "../admin-login.html"; // Replace with your login page URL
+  }
+}
+
+checkAuth();
+
+function logout() {
+  localStorage.removeItem("adminToken");
+  // Redirect to the login page
+  window.location.href = "../admin-login.html"; // Replace with your login page URL
+}
+
+document.getElementById("logout").addEventListener("click", logout);
